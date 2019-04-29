2c3f767bd5e1b24340fb3ccbf195eb85f972ead6
Guilherme <guilherme.francisquini@hotmail.com>
2019-04-28 22:17:16 -0300

    adiciona exemple 5

---
+++ 05-tipo_object_e_any.js
@@ -0,0 +1,16 @@
+var obj = {
+    first_name: 'Guilherme',
+    last_name: 'Francisquini'
+};
+// obj = "string";
+// let obj1: object = {
+//     category: 'categoria 1',
+//     product: 'produto 1',
+// };
+// obj1 = 2;
+var variable = 4;
+variable = {};
+variable = "string";
+variable = false;
+var myArray = [2, "string", false, { name: 'Guilherme Francisquini' }];
+var myArray2 = [2, "string", false, { name: 'Guilherme Francisquini' }];
---
+++ 05-tipo_object_e_any.ts
@@ -0,0 +1,25 @@
+let obj : object = {
+    first_name: 'Guilherme',
+    last_name: 'Francisquini',
+};
+
+// obj = "string";
+
+// let obj1: object = {
+//     category: 'categoria 1',
+//     product: 'produto 1',
+// };
+
+// obj1 = 2;
+
+
+let variable : any = 4;
+
+variable = {};
+
+variable = "string";
+
+variable = false;
+
+let myArray:Array<any> = [2, "string", false, {name: 'Guilherme Francisquini'}];
+let myArray2:any[] = [2, "string", false, {name: 'Guilherme Francisquini'}];
