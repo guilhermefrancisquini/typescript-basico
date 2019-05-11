d9ad376198d2cc5d32d17f6e82bd37b66a81356f
Guilherme <guilherme.francisquini@hotmail.com>
2019-05-11 00:18:39 -0300

    Adiciona exemplo 9

---
+++ 09-interfaces.js
@@ -0,0 +1,10 @@
+// function show({category, product}: { category: string, product: string }) {
+//     console.log(category);
+//     console.log(product);
+// }
+function show(_a) {
+    var category = _a.category, product = _a.product;
+    console.log(category);
+    console.log(product);
+}
+show({ category: "Categoria" });
---
+++ 09-interfaces.ts
@@ -0,0 +1,30 @@
+interface ObjectValue {
+    category: string;
+    product?: string;
+
+}
+
+// function show({category, product}: { category: string, product: string }) {
+//     console.log(category);
+//     console.log(product);
+// }
+
+function show({ category, product }: ObjectValue) {
+    console.log(category);
+    console.log(product);
+}
+
+show({ category: "Categoria" });
+
+let obj: ObjectValue = { category: "Categoria 1", product: "asdf" };
+
+class Product implements ObjectValue {
+    category: string;
+    // product: string;
+}
+
+let product =  new Product();
+product.category = 'Categoria';
+
+console.log(product.category);
+// console.log(product.product);
